ef645323005b4397a178a1627bf23dbd282d88f4
Audrey Wash <robinsonaudrey6106@gmail.com>
2024-08-28 11:46:24 -0500

    add assignment

---
+++ Introducing_Python/1.4B: Implementing User Input and Comments in a Script.py
@@ -0,0 +1,18 @@
+"""
+This script takes a whole number from the user and prints out its multiplication table 
+with multiples of 2 from 2 up to 10.
+"""
+
+whole_num = int(input("Generate a multiplication table for: "))
+
+print("__________")
+
+print("Number: " + str(whole_num))
+
+print("2: " + str(whole_num * 2))
+print("4: " + str(whole_num * 4))
+print("6: " + str(whole_num * 6))
+print("8: " + str(whole_num * 8))
+print("10: " + str(whole_num * 10))
+
+print("__________")

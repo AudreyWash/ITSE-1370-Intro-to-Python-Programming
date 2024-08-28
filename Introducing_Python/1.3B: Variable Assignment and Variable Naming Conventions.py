953262869b665a6afeccbf255cc248991f27d025
Audrey Wash <robinsonaudrey6106@gmail.com>
2024-08-28 11:38:49 -0500

    add assignment

---
+++ Introducing_Python/1.3B: Variable Assignment and Variable Naming Conventions.py
@@ -0,0 +1,11 @@
+PI = 3.14159
+radius = 3
+
+#Calculate diameter
+diameter = 2 * radius
+
+#Calculate aread
+area = PI * (radius ** 2)
+
+print(f"Diameter of the circle: {diameter}")
+print(f"Area of the circle: {area}")

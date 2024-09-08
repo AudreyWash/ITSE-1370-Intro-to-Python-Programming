ba1b86638e0a0f46cf157c63928f3b9689bc9770
Audrey Wash <robinsonaudrey6106@gmail.com>
2024-09-08 18:46:34 -0500

    edit project

---
+++ Introducing_Python/Lab Assessment 1: Unit Converter/main.py
@@ -0,0 +1,20 @@
+def convert_kg(value):
+	#converts kg to lbs and ounces
+	lbs = value*2.20462
+	oz = value*35.274
+	#displays kg into lbs and ounces
+	print(str(value) + " kg converted is "+ str(lbs) + " pounds and " + str(oz) + " ounces.")
+
+def convert_pounds(value):
+	#converts lbs to kg and ounces
+	kg = value*0.453592 
+	oz = value*16
+	#displays kg into lbs and ounces
+	print(str(value) + " pounds converted is "+ str(kg) + " kg and " + str(oz) + " ounces.")
+
+def convert_ounces(value):
+	#converts ounces to kg and pounds
+	kg = value* 0.0283 
+	lbs = value*0.0625
+	#displays ounces into lbs and kg
+	print(str(value) + " ounces converted is "+ str(kg) + " kg and " + str(lbs) + " pounds.") 
